c56af7ee70de46172709759674bffe9e55a0e190
maweber98 <weberdesignstudio@gmail.com>
2020-12-09 15:11:01 -0500

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -14,14 +14,6 @@ console.log('Expensify App!');
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10950 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
