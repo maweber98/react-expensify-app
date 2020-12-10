8e05fc8095cd2638b960d9ef5782f47a0639c601
maweber98 <weberdesignstudio@gmail.com>
2020-12-10 10:29:05 -0500

    New modified files - fixed text filter issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <ul>
-            <li>
-                <NavLink exact={true} activeClassName="is-active" to="/">Dashboard</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="is-active" to="/create">Create</NavLink>
-            </li>
-        </ul>
-    </header>
-)
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+  </header>
+);
 
 export default Header;
